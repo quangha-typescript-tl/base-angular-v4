9b3b6f776ce2589ff15b8d844b534bbd9764cac0
QuangHA <quangha@ominext.com>
2019-06-10 08:11:58 +0700

    init env

---
+++ src/environments/environment.develop.ts
@@ -0,0 +1,9 @@
+import {Profile} from '../app/shared/profile';
+
+export const environment = {
+  profile: Profile.develop,
+  production: false,
+  api: 'http://test.develop.com',
+  authApi: 'http://test.develop.com',
+  socket: 'http://test.develop.com',
+};
